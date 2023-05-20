8174084271acfe937d35dd7e51cb6d596a204024
Xavier Flix <morphx666@users.noreply.github.com>
2023-05-19 23:02:49 -0400

    Added support to convert value ranges

--- Program.cs
+++ Program.cs
@@ -10,26 +10,50 @@
                                 Usage:
                                     bc value fromBase toBase
 
-                                    value: the numeric value to convert
-                                    fromBase: the base in which the value is expressed
-                                    toBase: the desired target base
+                                    value: The numeric value to convert
+                                           It can also be defined as a range of values: a-b
+                                    fromBase: The base in which the value is expressed
+                                    toBase: The desired target base
 
                                 Example:
-                                    > bc 37 5 9
+                                    q> bc 37 5 9
                                         (convert 37 from base 5 to base 9)
-                                    > 37₅ = 24₉
+                                    r> 37₅ = 24₉
+
+                                    q> bc 7-12 11 3
+                                        (convert all values from 7 to 12 from base 11 to base 3)
+                                    r> 7₁₁ = 21₃
+                                    r> 8₁₁ = 22₃
+                                    r> 9₁₁ = 100₃
+                                    r> A₁₁ = 101₃
+                                    r> 10₁₁ = 102₃
+                                    r> 11₁₁ = 110₃
+                                    r> 12₁₁ = 111₃
                                 """);
                 Environment.Exit(1);
             }
 
-            string v1 = argv[0];
             int fb = int.Parse(argv[1]);
             int tb = int.Parse(argv[2]);
-            
-            string result = Convert(v1, fb, tb);
+            string v = argv[0];
+
+            if(v.Contains('-')) {
+                UInt64[] vs = v.Split('-').Select(x => ToBase10(x, fb)).ToArray();
+                for(UInt64 i = vs[0]; i <= vs[^1]; i++) {
+                    v = FromBase10(i, fb);
+                    PrintConversion(v, fb, tb);
+                    Console.WriteLine();
+                }
+            } else {
+                PrintConversion(v, fb, tb);
+            }
+        }
+
+        private static void PrintConversion(string v, int fb, int tb) {
+            string result = Convert(v, fb, tb);
 
             //Console.WriteLine($"{v1} b{fb} = {result} b{tb}");
-            string r = $"{v1}|{fb}| = {result}|{tb}|";
+            string r = $"{v}|{fb}| = {result}|{tb}|";
             bool isBase = false;
             for(int i = 0; i < r.Length; i++) {
                 if(r[i] == '|') {
@@ -64,8 +88,8 @@
             int len = value.Length - 1;
             for(int i = 0; i <= len; i++) {
                 int d = (int)value[i] - 48;
-                if(d >= fromBase) throw new ArgumentException($"{value} is not representable in base {fromBase}");
-                res += (UInt64)((d >= 10 ? d - 7 : d) * Math.Pow(fromBase, len - i));
+                UInt64 r = (UInt64)((d >= 10 ? d - 7 : d) * Math.Pow(fromBase, len - i));
+                res += r;
             }
 
             return res;
